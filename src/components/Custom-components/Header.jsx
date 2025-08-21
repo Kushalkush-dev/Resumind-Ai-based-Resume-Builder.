92258e985b147cb9d417bcf50e1b92e37c2ad306
Kushal J <jkushal208@gmail.com>
2025-08-21 14:13:14 +0530

    add route link button  based on the path

--- src/components/Custom-components/Header.jsx
+++ src/components/Custom-components/Header.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
+import { UserButton, useUser } from '@clerk/clerk-react'
 
 const Header = () => {
+  const {user,isSignedIn}=useUser()
   return (
     <>
     <div className='flex w-full justify-center items-center'>
     <div className='flex justify-between p-2 shadow-2xl h-max mt-1.5  rounded-2xl w-4/5 '>
       <img src="logo.svg" alt="logo" width={150} height={150} />
-      
+
+      {!isSignedIn?(<Link to={'/auth/sign-in'}>
       <Button variant="outline">Get Started</Button>
+      
+      </Link> )
+      :(
+        <Link to={'/dashboard'}>
+        <div className='flex items-center gap-1'>
+          <Button variant="outline">Dashboard</Button>
+          <UserButton/>
+        </div>
+
+        </Link>
+      )}
+      
 
     </div>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import Header from '@/components/Custom-components/Header'
+import { useUser } from '@clerk/clerk-react'
 import React from 'react'
 
 const Home = () => {
+
+  const {user,isSignedIn}=useUser()
   return (
     <div>
       <Header/>
