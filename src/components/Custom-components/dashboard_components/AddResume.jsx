b72cd3f99331a45ea2efd1926716def770bd4d81
Kushal J <jkushal208@gmail.com>
2025-08-22 18:40:22 +0530

    add dakboard layout with add resume componenet

---
+++ src/components/Custom-components/dashboard_components/AddResume.jsx
@@ -0,0 +1,15 @@
+import { PlusCircle } from 'lucide-react'
+import React from 'react'
+
+const AddResume = () => {
+  return (
+    <div >
+  <div className='px-14 py-30 border flex items-center justify-center bg-secondary'>
+    <PlusCircle/>
+  </div>
+
+    </div>
+  )
+}
+
+export default AddResume
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,19 @@
+import AddResume from '@/components/Custom-components/dashboard_components/AddResume'
 import { UserButton } from '@clerk/clerk-react'
 import React from 'react'
 
 const Dashboard = () => {
   return (
     <>
-    <UserButton/>
-    <div>Dashboard</div>
+
+    <div className='p-10 md:px-20 lg:px-32'>
+      <h2 className='font-bold text-3xl'>My Resume</h2>
+      <p className='font-semibold'>Build your resume without hassle</p>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        <AddResume/>
+      </div>
+    </div>
+   
     </>
   )
 }
