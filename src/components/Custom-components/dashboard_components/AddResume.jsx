722b97ae37f724a77454a1255ad33b1aa337d1c8
Kushal J <jkushal208@gmail.com>
2025-08-22 19:05:45 +0530

    add open on div click

--- src/components/Custom-components/dashboard_components/AddResume.jsx
+++ src/components/Custom-components/dashboard_components/AddResume.jsx
@@ -1,13 +1,37 @@
 import { PlusCircle } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 const AddResume = () => {
+  const [openDialog, setopenDialog] = useState(false)
+
   return (
     <div >
-  <div className='px-14 py-30  flex items-center justify-center bg-secondary border-dashed border-2 
+    <div onClick={()=>setopenDialog(true)} className='px-14 py-30  flex items-center justify-center bg-secondary border-dashed border-2 
                   rounded-lg h-[282px] cursor-pointer hover:scale-105 hover:shadow-lg transition-all'>
+
     <PlusCircle/>
+
   </div>
+
+    <Dialog open={openDialog}>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Are you absolutely sure?</DialogTitle>
+        <DialogDescription>
+          This action cannot be undone. This will permanently delete your account
+          and remove your data from our servers.
+        </DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
 
     </div>
   )
