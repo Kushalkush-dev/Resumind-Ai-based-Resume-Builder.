186376367e5bb52b614138a9e17e3dfff48abfc5
Kushal J <jkushal208@gmail.com>
2025-08-22 18:48:07 +0530

    update styles of add resume component

--- src/components/Custom-components/dashboard_components/AddResume.jsx
+++ src/components/Custom-components/dashboard_components/AddResume.jsx
@@ -4,7 +4,8 @@ import React from 'react'
 const AddResume = () => {
   return (
     <div >
-  <div className='px-14 py-30 border flex items-center justify-center bg-secondary'>
+  <div className='px-14 py-30  flex items-center justify-center bg-secondary border-dashed border-2 
+                  rounded-lg h-[282px] cursor-pointer hover:scale-105 hover:shadow-lg transition-all'>
     <PlusCircle/>
   </div>
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
     <div className='p-10 md:px-20 lg:px-32'>
       <h2 className='font-bold text-3xl'>My Resume</h2>
       <p className='font-semibold'>Build your resume without hassle</p>
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-10'>
         <AddResume/>
       </div>
     </div>
