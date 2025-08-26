cf162374724671826fdc084a57bd4a5896ad7a9d
Kushal J <jkushal208@gmail.com>
2025-08-26 15:50:23 +0530

    add the personal detail section

--- src/components/Custom-components/ResumePreview components/Resumepreview.jsx
+++ src/components/Custom-components/ResumePreview components/Resumepreview.jsx
@@ -7,8 +7,16 @@ const Resumepreview = () => {
     <div>
       {/* personal info */}
 
-      <div className=''>
-        <h2>{resumeInfo.firstName} {resumeInfo.lastName}</h2>
+      <div className='shadow-lg p-10 border-t-[20px] h-full '
+      style={{borderColor:resumeInfo.themeColor}}>
+        <h2 className='text-center font-medium text-xl'>{resumeInfo.firstName} {resumeInfo.lastName}</h2>
+        <h2 className='text-center font-medium text-xs'>{resumeInfo.jobTitle}</h2>
+        <h2 className='text-center font-medium text-xs'>{resumeInfo.address}</h2>
+
+        <div className='flex justify-between pt-7'>
+          <h2>{resumeInfo.phone}</h2>
+          <h2>{resumeInfo.email}</h2>
+        </div>
       </div>
 
       {/* summary */}
