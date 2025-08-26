11da0f9a876cc3294638ce9f50ba7932440aa936
Kushal J <jkushal208@gmail.com>
2025-08-26 16:43:46 +0530

    add experience section

--- src/components/Custom-components/ResumePreview components/Resumepreview.jsx
+++ src/components/Custom-components/ResumePreview components/Resumepreview.jsx
@@ -9,7 +9,7 @@ const Resumepreview = () => {
 
       <div className='shadow-lg p-10 border-t-[20px] h-full '
       style={{borderColor:resumeInfo.themeColor}}>
-        <h2 className='text-center font-medium text-xl'>{resumeInfo.firstName} {resumeInfo.lastName}</h2>
+        <h2 className='text-center font-medium text-2xl'>{resumeInfo.firstName} {resumeInfo.lastName}</h2>
         <h2 className='text-center font-medium text-xs'>{resumeInfo.jobTitle}</h2>
         <h2 className='text-center font-medium text-xs'>{resumeInfo.address}</h2>
 
@@ -18,13 +18,29 @@ const Resumepreview = () => {
           <h2>{resumeInfo.email}</h2>
         </div>
       
-    <hr className='border-2 my-1.5' style={{borderColor:resumeInfo.themeColor}}/>
+       <hr className='border-2 my-1.5' style={{borderColor:resumeInfo.themeColor}}/>
       {/* summary */}
 
       <p className='text-xs font-medium'>{resumeInfo.summary}</p>
 
       {/* experience */}
+      <div className='my-2'>
+      <h2 className='font-sans font-medium text-lg text-center'>Experience</h2>
+      <hr className='border-2' style={{borderColor:resumeInfo.themeColor}}/>
 
+      {resumeInfo.experience.map((experience,index)=>(
+        <div className="my-2.5" key={index}>
+          <h2 className='text-md font-medium'>{experience.title}</h2>
+          <div className='flex justify-between'>
+            <h3 className='text-xs'>{experience.companyName},{experience.city},{experience.state}</h3>
+            <h3 className='text-xs'>{experience.startDate},{experience.endDate?experience.endDate:"Present"}</h3>
+          </div>
+           <p className='text-xs font-medium my-2'>{experience.workSummary}</p>
+        </div>
+        
+
+      ))}
+      </div>
       {/* education */}
 
       {/* skills */}
