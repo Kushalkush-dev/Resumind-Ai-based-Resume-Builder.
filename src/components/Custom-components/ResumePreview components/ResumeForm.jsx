2970a1b6baadcc7f67ed7724440b030e42ddba6e
Kushal J <jkushal208@gmail.com>
2025-08-26 19:33:23 +0530

    render personal details section based on the activeformsection

--- src/components/Custom-components/ResumePreview components/ResumeForm.jsx
+++ src/components/Custom-components/ResumePreview components/ResumeForm.jsx
@@ -24,7 +24,7 @@ const ResumeForm = () => {
       </div>
 
           {/* personal details */}
-          <Personaldetails/>
+         {activeformsection==1 && <Personaldetails/>} 
 
           {/* summary details */}
 
