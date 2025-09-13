801b08bf8c282904d101eb6092c684bbbd9b18e9
Kushal J <jkushal208@gmail.com>
2025-09-13 18:50:23 +0530

    add the delete alert dialog before deleting

--- src/components/Custom-components/dashboard_components/ExistingResume.jsx
+++ src/components/Custom-components/dashboard_components/ExistingResume.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Link, Links, useNavigate } from 'react-router-dom'
+import { MoreVertical } from 'lucide-react'
+import globalApi from './../../../../service/globalApi'
+import { toast } from 'sonner'
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,11 +12,18 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { MoreVertical } from 'lucide-react'
-import globalApi from './../../../../service/globalApi'
-import { toast } from 'sonner'
 
-
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 
 const ExistingResumes = ({resumedetail,ResumeNum}) => {
@@ -37,7 +48,7 @@ const ExistingResumes = ({resumedetail,ResumeNum}) => {
 
 
   return (
-    <Link to={`/dashboard/resume/${resumedetail.documentId}/edit`}>
+    
       <div className='relative'>
       <div className='px-14 py-30  flex items-center justify-center bg-secondary border-red-500 border-2 
                   rounded-lg h-[282px] cursor-pointer hover:scale-105 hover:shadow-lg transition-all'>
@@ -66,7 +77,7 @@ const ExistingResumes = ({resumedetail,ResumeNum}) => {
       </div>
                   
     </div>
-    </Link>
+   
     
   )
 }
