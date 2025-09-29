dbe1bc8e240b5e9d39561e12d51bc39aec2c9413
Kushal J <jkushal208@gmail.com>
2025-09-29 19:37:16 +0530

    and homepage link to logo

--- src/components/Custom-components/Header.jsx
+++ src/components/Custom-components/Header.jsx
@@ -10,7 +10,9 @@ const Header = () => {
     <>
     <div className='flex w-full justify-center  items-center'>
     <div className='flex justify-between p-2 shadow-md  mt-1.5 bg-white rounded-2xl w-4/5 '>
-    <img src="/logo.svg" alt="logo" width={150} height={150} />
+    <Link to='/'>
+    <img src="/logo.svg" alt="logo" width={150} height={150}  />
+    </Link>
 
       {!isSignedIn?(<Link to={'/auth/sign-in'}>
       <Button className="bg-[#45D2B0] hover:bg-[#2dad90]">Get Started</Button>
